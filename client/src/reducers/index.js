ed8155ed6988d703d3fc62940f3ed5a462a60a9d
Martin Micheletti <martinmicheletti@hotmail.com>
2021-08-29 21:05:14 -0300

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
 
 import posts from "./posts";
 
-export default combineReducers({
-  posts,
-});
+export default (history) =>
+  combineReducers({
+    router: connectRouter(history),
+    posts,
+  });
